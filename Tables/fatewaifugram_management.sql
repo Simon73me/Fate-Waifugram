945de3e2c7a8158dce91ce5f641237d4b56d9dbc
Simon73me <38844632+Simon73me@users.noreply.github.com>
2020-06-15 23:04:07 +0200

    Add files via upload

---
+++ Tables/fatewaifugram_management.sql
@@ -0,0 +1,47 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fatewaifugram
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `management`
+--
+
+DROP TABLE IF EXISTS `management`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `management` (
+  `ID_Supergroup` bigint NOT NULL,
+  `ID_Servant` int DEFAULT NULL,
+  `Time_mess` int unsigned NOT NULL,
+  `Started` tinyint NOT NULL,
+  `Time_reset` int unsigned NOT NULL,
+  PRIMARY KEY (`ID_Supergroup`),
+  KEY `management_ibfk_2` (`ID_Servant`),
+  CONSTRAINT `management_ibfk_1` FOREIGN KEY (`ID_Supergroup`) REFERENCES `supergroups` (`ID_Supergroup`),
+  CONSTRAINT `management_ibfk_2` FOREIGN KEY (`ID_Servant`) REFERENCES `servants` (`ID_Servant`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-15 23:02:43
---
+++ Tables/fatewaifugram_trades.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fatewaifugram
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `trades`
+--
+
+DROP TABLE IF EXISTS `trades`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `trades` (
+  `ID_Supergroup` bigint NOT NULL,
+  `Mess_ID_Trade` bigint NOT NULL,
+  `ID_User_1` bigint NOT NULL,
+  `ID_User_2` bigint NOT NULL,
+  `Trade_1` int NOT NULL,
+  `Trade_2` int NOT NULL,
+  PRIMARY KEY (`ID_Supergroup`,`Mess_ID_Trade`),
+  KEY `ID_User_1` (`ID_User_1`),
+  KEY `ID_User_2` (`ID_User_2`),
+  KEY `Trade_1` (`Trade_1`),
+  KEY `Trade_2` (`Trade_2`),
+  CONSTRAINT `trades_ibfk_1` FOREIGN KEY (`ID_User_1`) REFERENCES `users` (`ID_User`),
+  CONSTRAINT `trades_ibfk_2` FOREIGN KEY (`ID_User_2`) REFERENCES `users` (`ID_User`),
+  CONSTRAINT `trades_ibfk_3` FOREIGN KEY (`ID_Supergroup`) REFERENCES `supergroups` (`ID_Supergroup`),
+  CONSTRAINT `trades_ibfk_4` FOREIGN KEY (`Trade_1`) REFERENCES `servants` (`ID_Servant`),
+  CONSTRAINT `trades_ibfk_5` FOREIGN KEY (`Trade_2`) REFERENCES `servants` (`ID_Servant`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-06-15 23:02:44
